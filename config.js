fa14dadb69896e34b34a2bfe790bb573ec94a232
ini-coder-vibe <williamsiniya63@gmail.com>
2026-03-14 16:17:46 +0100

    Add files via upload

---
+++ config.js
@@ -0,0 +1,6 @@
+const CONFIG = {
+  SUPABASE_URL:             'https://yourproject.supabase.co',
+  SUPABASE_PUBLISHABLE_KEY: 'your_key',
+  SUPABASE_SERVICE_KEY: 'your_service_key',
+  ADMIN_TOKEN: 'your_token'
+};
